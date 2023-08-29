084d5fb39be47ead7f6a179db8f20a47b6220f1a
Дмитрий Гусев <mitai3016@gmail.com>
2023-08-29 17:27:35 +0300

    Initial commit

---
+++ BinaryViewer/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace BinaryViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReadFile = new System.Windows.Forms.Button();
+            this.textBoxContent = new System.Windows.Forms.RichTextBox();
+            this.buttonFindOffset = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonReadFile
+            // 
+            this.buttonReadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReadFile.Location = new System.Drawing.Point(1039, 27);
+            this.buttonReadFile.Name = "buttonReadFile";
+            this.buttonReadFile.Size = new System.Drawing.Size(265, 39);
+            this.buttonReadFile.TabIndex = 1;
+            this.buttonReadFile.Text = "Открыть файл";
+            this.buttonReadFile.UseVisualStyleBackColor = true;
+            this.buttonReadFile.Click += new System.EventHandler(this.ButtonReadFile_Click);
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxContent.Location = new System.Drawing.Point(37, 97);
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.ReadOnly = true;
+            this.textBoxContent.Size = new System.Drawing.Size(1267, 605);
+            this.textBoxContent.TabIndex = 2;
+            this.textBoxContent.Text = "";
+            // 
+            // buttonFindOffset
+            // 
+            this.buttonFindOffset.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFindOffset.Location = new System.Drawing.Point(274, 27);
+            this.buttonFindOffset.Name = "buttonFindOffset";
+            this.buttonFindOffset.Size = new System.Drawing.Size(342, 39);
+            this.buttonFindOffset.TabIndex = 3;
+            this.buttonFindOffset.Text = "Перейти к смещению";
+            this.buttonFindOffset.UseVisualStyleBackColor = true;
+            this.buttonFindOffset.Click += new System.EventHandler(this.FindOffset_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(78, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 38);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCancel.Location = new System.Drawing.Point(748, 27);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(270, 39);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Прервать чтение";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(31, 30);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(44, 32);
+            this.label.TabIndex = 6;
+            this.label.Text = "0x";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 739);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonFindOffset);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.buttonReadFile);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonReadFile;
+        private System.Windows.Forms.RichTextBox textBoxContent;
+        private System.Windows.Forms.Button buttonFindOffset;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label;
+    }
+}
---
+++ BinaryViewer/MainForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BinaryViewer
+
+{
+
+    public partial class MainForm : Form
+    {
+        private string filePath;
+
+        OpenFileDialog openFile = new OpenFileDialog();
+        CancellationTokenSource _tokenSource;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private delegate void UpdateTextDelegate(string text);
+        private delegate void ChangeStatus(Button b, bool status);
+
+        //Чтение файла
+        private async void WriteTextAsynchronously()
+        {
+
+            _tokenSource = new CancellationTokenSource();
+            CancellationToken cancelToken = _tokenSource.Token;
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxContent.Clear();
+                    filePath = openFile.FileName;
+                }
+                else return;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    using (var reader = new StreamReader(filePath))
+                    {
+                        byte[] buffer = new byte[16];
+                        int bytesRead;
+                        long offset = 0;
+
+                        StringBuilder charBuilder = new StringBuilder();
+                        ProgramStatusRead();
+
+                        while ((bytesRead = reader.BaseStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            string text = $"0x{offset:X8}: ";
+
+                            for (int i = 0; i < bytesRead; i++)
+                            {
+                                text += $"{buffer[i]:X2} ";
+                                byte b = buffer[i];
+                                if (b < 32 || b > 127) charBuilder.Append('.');
+                                else charBuilder.Append((char)b);
+
+                                if (i == bytesRead / 2 - 1)
+                                    text += "| ";
+                                if (i == bytesRead - 1)
+                                {
+                                    text += "| ";
+                                    text += charBuilder.ToString();
+                                    charBuilder.Clear();
+                                }
+                            }
+
+                            text += Environment.NewLine;
+
+                            Invoke(new UpdateTextDelegate(UpdateTextBoxContent), text);
+
+                            offset += bytesRead;
+                            cancelToken.ThrowIfCancellationRequested();
+
+                        }
+                        ProgramStatusNormal();
+                    }
+                }
+                //обработка исключения при отмене чтения
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ProgramStatusNormal();
+                }
+            });
+        }
+        //Изменение состояния кнопок при чтении файла
+        private void ProgramStatusRead()
+        {
+            Invoke(new ChangeStatus(ChangeButtonStatus), buttonReadFile, false);
+            Invoke(new ChangeStatus(ChangeButtonStatus), buttonFindOffset, false);
+            Invoke(new ChangeStatus(ChangeButtonStatus), buttonCancel, true);
+        }
+        //Изменение состояния кнопок если чтение не происходит
+        private void ProgramStatusNormal()
+        {
+            Invoke(new ChangeStatus(ChangeButtonStatus), buttonReadFile, true);
+            Invoke(new ChangeStatus(ChangeButtonStatus), buttonFindOffset, true);
+            Invoke(new ChangeStatus(ChangeButtonStatus), buttonCancel, false);
+        }
+        private void ChangeButtonStatus(Button b, bool status)
+        {
+            b.Enabled = status;
+        }
+
+        //Обновление текстового поля
+        private void UpdateTextBoxContent(string text)
+        {
+            textBoxContent.AppendText(text);
+        }
+
+        //Кнопка чтения
+        private void ButtonReadFile_Click(object sender, EventArgs e)
+        {
+            WriteTextAsynchronously();
+        }
+        //Кнопка поиска по смщенеию
+        private void FindOffset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = 0;
+                string needFind = textBox1.Text;
+                if (needFind.Length != 8) throw new Exception("Неверно введённый формат");
+                index = textBoxContent.Find(needFind, index, RichTextBoxFinds.MatchCase);
+                if (index != -1)
+                {
+                    textBoxContent.SelectionStart = index;
+                    textBoxContent.ScrollToCaret();
+                    textBoxContent.Focus();
+                }
+                else
+                {
+                    throw new Exception("Смещение не найдено");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //Кнопка для отмены чтения
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            _tokenSource.Cancel();
+        }
+    }
+}
+
